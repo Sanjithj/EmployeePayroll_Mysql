710f4b41e6cbc9ddd226d76d64e7879d62e954cb
Sanjithj <sanjithj591997@gmail.com>
2022-07-30 13:07:38 +0530

    Added: create Employee Payroll database

--- EmpDatabase.java
+++ EmpDatabase.java
@@ -3,5 +3,9 @@ package com.bridgelabz;
 public class EmpDatabase {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee DataBase");
+	
+	create database Employee_Payroll;
+    show databases;
+    use Employee_PayRoll;
 }
 }
