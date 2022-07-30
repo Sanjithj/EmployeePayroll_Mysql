dbebe49e4db93829c67907e2b1e4be3717087de2
Sanjithj <sanjithj591997@gmail.com>
2022-07-30 13:24:43 +0530

    Added: Insert into database

--- EmpDatabase.java
+++ EmpDatabase.java
@@ -9,5 +9,8 @@ public static void main(String[] args) {
     use Employee_PayRoll;
     
     create table employee_payroll( id int NOT NULL AUTO_INCREMENT PRIMARY KEY,name varchar(30),salary INT,start_date DATE NOT NULL);
+
+    insert into employee_payroll(name,salary,start_date) values("Sanjith",560000,"05-08-22");
+    insert into employee_payroll(name,salary,start_date) values("Ragav",660000,"25-07-22");
 }
 }
